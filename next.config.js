f06d412ba04be973050e840331fdb52abf9fbc2e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-07-25 15:45:08 +0900

    [add] eslintの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    eslint: {
+        dirs: ['app', 'features'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    },
 }
 
 module.exports = nextConfig
