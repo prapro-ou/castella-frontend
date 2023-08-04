43baa0b28432c2a4813d00cfefe779a314ea59c9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-04 16:05:48 +0900

    [format] prettierを実行

--- features/components/UnreadMessageCount.js
+++ features/components/UnreadMessageCount.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //This component shows number of unread message
 //count:number of unread message
-export default function UnreadMessageCount({ count })  {
+export default function UnreadMessageCount({ count }) {
   /*Nothing shown count is zero
   if(count == 0){
     return null;
@@ -10,13 +10,13 @@ export default function UnreadMessageCount({ count })  {
   */
   //Show '9+'
   //I don't know its helpful
-  if(count > 9){
-    count = "9+";
+  if (count > 9) {
+    count = '9+';
   }
   //rentering
   return (
-    <div className="rounded-full h-10 w-10 flex justify-center items-center bg-primary text-white">
-        <span>{count}</span>
+    <div className='flex h-10 w-10 items-center justify-center rounded-full bg-primary text-white'>
+      <span>{count}</span>
     </div>
   );
-};
+}
