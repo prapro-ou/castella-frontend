ecabc2c657f844b716388d420762eddf1aab36d1
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-04 18:07:49 +0900

    コメント訂正

--- features/components/UnreadMessageCount.js
+++ features/components/UnreadMessageCount.js
@@ -2,15 +2,12 @@
 import React from 'react';
 
 /**
- * This component shows number of unread message
+ * 未読数を表示するためのコンポーネント
  * @param {count} *number of unread message
  * @author Jaja-UMA
 */
 export default function UnreadMessageCount({ count })  {
-  /**
-   * Show '9+'or count
-   * @type {showCount} 未読数として表示する値
-   */
+
   const showCount = (count > 9)? '9+' : count;
   return (
     <div className="flex h-10 w-10 items-center justify-center rounded-full bg-primary text-white">
