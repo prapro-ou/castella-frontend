548d4fcc81a470241a768d517334e6beb04460f7
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-04 18:40:00 +0900

    [move]コード修正

---
+++ features/talks/threads/messages/components/Message.js
@@ -0,0 +1,17 @@
+"use client";
+import React, { useState } from 'react';
+
+/**
+ * メッセージの枠組みを作り，メッセージを表示
+ * @param {value} 表示するメッセージ
+ */
+export default function Message({ value })  {
+    const [isExpanded,setIsExpanded] = useState(false);
+    const messageNum = value.length;
+    const displayText = (isExpanded==false && messageNum > 100) ? value.substr(0,100)+"..." : value;
+  return (
+    <button onClick={()=> setIsExpanded(!isExpanded)} className="mx-auto mt-4 w-96 overflow-hidden whitespace-pre-line rounded-lg bg-primary p-4 text-left text-white shadow-md">
+      {displayText}
+  </button>
+  );
+};
