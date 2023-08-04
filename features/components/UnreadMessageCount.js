7390f01d689106c4603825ff5ac7991e0c72ffa6
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-04 15:47:21 +0900

    実装

--- features/components/UnreadMessageCount.js
+++ features/components/UnreadMessageCount.js
@@ -3,14 +3,15 @@ import React from 'react';
 //This component shows number of unread message
 //count:number of unread message
 export default function UnreadMessageCount({ count })  {
-  //Nothing shown count is zero
+  /*Nothing shown count is zero
   if(count == 0){
-    return null
+    return null;
   }
+  */
   //Show '9+'
   //I don't know its helpful
   if(count > 9){
-    count = "9+"
+    count = "9+";
   }
   //rentering
   return (
