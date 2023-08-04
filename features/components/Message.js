463f0fcdcf1e07834803a6fc804f7d8e61e9a6ab
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-04 15:52:31 +0900

    不必要なコード削除

--- features/components/Message.js
+++ features/components/Message.js
@@ -8,16 +8,8 @@ export default function Message({ value })  {
     if(isExpanded==false && messageNum > 100){
       displayText=value.substr(0,100)+"...";
     }
-  //rentering
   return (
-    /*
-    <div className="mx-auto mt-4 max-w-sm overflow-hidden rounded-lg bg-thin shadow-md  ">
-        <div className="p-4">
-                <p  className=" whitespace-pre-line">{value}</p>
-        </div>
-    </div>
-    */
-    <button onClick={()=> setIsExpanded(!isExpanded)} className="mx-auto mt-4 w-96 overflow-hidden whitespace-pre-line rounded-lg bg-primary p-4 text-white text-left shadow-md">
+    <button onClick={()=> setIsExpanded(!isExpanded)} className="mx-auto mt-4 w-96 overflow-hidden whitespace-pre-line rounded-lg bg-primary p-4 text-left text-white shadow-md">
       {displayText}
   </button>
 
