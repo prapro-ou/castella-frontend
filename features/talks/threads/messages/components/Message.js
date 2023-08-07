33ff9aa0e244ee28be9196853e3e0373b665f562
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-07 14:09:02 +0900

    省略に関する機能削除

--- features/talks/threads/messages/components/Message.js
+++ features/talks/threads/messages/components/Message.js
@@ -1,22 +1,16 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 /**
  * メッセージの枠組みを作り，メッセージを表示
  * @param {value} 表示するメッセージ
  */
 export default function Message({ value }) {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const messageNum = value.length;
-  const displayText =
-    isExpanded === false && messageNum > 100
-      ? value.slice(0, 100) + '...'
-      : value;
+  const displayText =value;
   return (
-    <button
-      onClick={() => setIsExpanded(!isExpanded)}
+    <div
       className=' w-96 overflow-hidden whitespace-pre-line rounded-lg bg-primary p-4 text-left text-white shadow-md'
     >
       {displayText}
-    </button>
+    </div>
   );
 }
