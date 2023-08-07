928bf0890dcaa295adbe4e140767cf1fd8926dc9
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-07 14:11:19 +0900

    displayText削除

--- features/talks/threads/messages/components/Message.js
+++ features/talks/threads/messages/components/Message.js
@@ -5,12 +5,11 @@ import React from 'react';
  * @param {value} 表示するメッセージ
  */
 export default function Message({ value }) {
-  const displayText =value;
   return (
     <div
       className=' w-96 overflow-hidden whitespace-pre-line rounded-lg bg-primary p-4 text-left text-white shadow-md'
     >
-      {displayText}
+      {value}
     </div>
   );
 }
