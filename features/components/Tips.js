80f59fbb42a8e31f650cbc8149be7fb26fd507ad
Yuto-Kambara <135821880+Yuto-Kambara@users.noreply.github.com>
2023-08-07 19:52:29 +0900

    幅を調整し中央に寄せる

--- features/components/Tips.js
+++ features/components/Tips.js
@@ -2,16 +2,15 @@ import Image from 'next/image';
 
 export default function Tips() {
   return (
-    <div className='w-fit'>
-      <div className='flex justify-center'>
-        <Image
-          width={40}
-          height={40}
-          alt='ハテナマーク'
-          src='/question_thin.svg'
-        />
-      </div>
-      <div className='text-thin flex justify-center'>左欄から選択できます</div>
+    <div className='flex w-fit items-center flex-col'>
+      <Image
+        width={40}
+        height={40}
+        alt='ハテナマーク'
+        src='/question_thin.svg'
+      />
+
+      <div className='text-thin'>左欄から選択できます</div>
     </div>
   );
 }
