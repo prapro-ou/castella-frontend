b39ec4d5a79dee3cd2ec1040bb6382e6efc2d227
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-08 18:39:35 +0900

    テスト，コード調整

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 'use client';
+import MessageTile from "@/features/talks/threads/messages/components/MessageTile";
 export default function Home() {
+  const dispMessage='おつかれ，進捗どう？すすんでるよね？aaaaあ進んでないいていったら貸す貸す貸すカステラ';
+  const at='12:30';
+  const alt='上aaa司';
   return (
     <main>
+      <MessageTile value={dispMessage} createdAt={at} alt={alt}/>
       <p>
         LAページ
         <a href='/login' className='text-primary'>
--- features/talks/threads/messages/components/MessageTile.js
+++ features/talks/threads/messages/components/MessageTile.js
@@ -1,23 +1,23 @@
 'use client';
-import Icon from '@/features/components/Icon';
 import React from 'react';
 import Message from './Message';
-
+import Icon from '@/features/components/Icon';
 
 /**
  * メッセージそのものを表すコンポーネント
  * @param {} src IconのURL
  * @param {} value メッセージ内容
- * @param {} created_at メッセージ作成日時
+ * @param {} createdAt メッセージ作成日時
+ * @param {} alt 画像が表示されなかった場合に表示される文字
  */
-export default function MessageTile({src,value,created_at}){
+export default function MessageTile({src,value,createdAt,alt}){
     return(
         <div className='flex' >
-            <div><Icon src={src}/></div>
+            <Icon src={src} alt={alt}/>
             <div className='ml-5'/>
-            <div><Message value={value}/></div>
+            <Message value={value}/>
             <div className='flex flex-col-reverse'>
-                <div className='text-xs text-thin ml-2'>{created_at}</div>
+                <div className='text-xs text-thin ml-2'>{createdAt}</div>
             </div>
         </div>
     );
