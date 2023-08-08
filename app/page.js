ceee59f24807d0082253899f76c11bef0307c472
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-08 11:58:56 +0900

    page.jsから不必要なコード削除

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
 'use client';
-import MessageTile from "@/features/talks/threads/messages/components/MessageTile";
-
 export default function Home() {
-  const dispMessage='おつかれ，進捗どう？すすんでるよね？';
-  const at='12:30';
   return (
     <main>
-      <MessageTile value={dispMessage} created_at={at}/>
-
       <p>
         LAページ
         <a href='/login' className='text-primary'>
