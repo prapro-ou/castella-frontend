c9232904357075c0913252d9a9c457e56c147b78
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-08 17:08:59 +0900

    選択時のアイコンの色反転以外完了

--- features/talks/threads/messages/components/DestinationTile.js
+++ features/talks/threads/messages/components/DestinationTile.js
@@ -4,30 +4,30 @@ import Icon from "@/features/components/Icon";
 
 export default function DestinationTile({ src, alt, name, messageCount, selected }) {
     return(
-        <div className={`relative flex h-6 w-36 rounded-lg ${selected ? 'bg-primary' : ''}`}>
+        <div className={`relative flex h-8 w-36 items-center rounded-lg ${selected ? 'bg-primary' : ''}`}> {/* ` バッククォートと ' シングルクォートの違いに注意*/}
             <div>
-            <Icon src={src} alt={alt} />
+                <Icon src={src} alt={alt} />{/*選択時の色変更は未実装 */}
             </div>
             <div className={`absolute left-6 ${selected ? 'text-white' : 'text-default'}`}>
                 {name}
             </div>
-            <div className={`absolute left-24`}>
-                <UnrMsgeCntForDT count={messageCount} size={6} isSelected={selected} />
-                {//<UnreadMessageCount count={messageCount}>
-                }
+            <div className={`absolute left-28`}>
+                <UnreadMessageCountForDT count={messageCount} size='6' isSelected={selected} />
+                {/*<UnreadMessageCount count={messageCount}>*/}
             </div>
         </div>
 
     );
 }
 
-function UnrMsgeCntForDT({ count,size,isSelected }) {
+function UnreadMessageCountForDT({ count,size,isSelected }) { 
+    {/*UnreadMessageCountForDestinationTile 元の関数内のマジックナンバーを書き換えて良いか判断しかねたので代替案として実装 */ }
     const showCount = count > 9 ? '9+' : count;
     const H_SIZE = "h-" + size;
     const W_SIZE = "w-" + size;
     return (
-      <div className={`flex ${H_SIZE} ${W_SIZE} items-center justify-center ${isSelected ? 'bg-white' : 'bg-primary'} ${isSelected ? 'text-primary':'text-white'} rounded-full`}>
-        {showCount}
-      </div>
+        <div className={`flex ${H_SIZE} ${W_SIZE} items-center justify-center ${isSelected ? 'bg-white' : 'bg-primary'} ${isSelected ? 'text-primary':'text-white'} rounded-full`}>
+            {showCount}
+        </div>
     );
-  }
+    }
