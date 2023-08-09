563aa5287beb4ac74d624ff56e2a4ab5e3c8c240
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-09 23:02:01 +0900

    [ad]引数の配列に関する処理の記述

--- features/talks/threads/messages/components/MessageScreen.js
+++ features/talks/threads/messages/components/MessageScreen.js
@@ -5,12 +5,20 @@ import MessageTile from './MessageTile';
 /**
  * メッセージと返信欄を示すためのコンポーネント
  * @param
+ * @param {string} src IconのURL
+ * @param {string} value メッセージ内容
+ * @param {string} createdAt メッセージ作成日時
+ * @param {string} alt 画像が表示されなかった場合に表示される文字
+ * @param {} onClickCreateReplyButton ボタンを押した時に動かす関数
  */
 
-export default function MessageScreen({message,onClickCreateReplyButton}){
+export default function MessageScreen([{messages,onClickCreateReplyButton}){
+
     return(
         <div>
-            aa
+            {messages.map((massage)=> (
+                <MessageTile key={massage.id} value={massage.value} src={massage.src} alt={massage.alt} />
+            ))}
         </div>
     );
 }
