e685c10a7cb700c8aec552bc7a25121cb5c0025f
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-09 10:58:18 +0900

    [ad+delete]Icon.jsへCSS要素の使と，テストコード削除

--- features/components/Icon.js
+++ features/components/Icon.js
@@ -1,15 +1,12 @@
 import Image from 'next/image';
-
 export default function Icon({ src, alt }) {
   if (src === undefined)
   {
-    console.log('案デファインド${src}');
-    console.log(src);
     return <div className='h-11 w-11 rounded-full bg-thin'></div>;
   }
   return (
     <Image
-      className='rounded-full'
+      className='rounded-full h-11 w-11'
       width={36}
       height={36}
       src={src}
