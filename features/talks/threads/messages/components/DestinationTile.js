aa47365f27562f35991784f843ee551a9803ff0d
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-10 03:50:46 +0900

    背景と文字の色の条件分岐を関数冒頭にまとめた

--- features/talks/threads/messages/components/DestinationTile.js
+++ features/talks/threads/messages/components/DestinationTile.js
@@ -5,12 +5,14 @@ import UnreadMessageCount from "@/features/components/UnreadMessageCount";
 export default function DestinationTile({ name, messageCount, selected }) {
     const src = selected ? './personIcon_white.svg' : './personIcon_black.svg';
     const alt = 'personIcon';
+    const bgColor = selected ? 'bg-primary':'';
+    const textColor = selected ? 'text-white':'text-default';
     return(
-        <div className={`relative flex h-12 w-64 items-center rounded-full p-3 ${selected ? 'bg-primary' : ''}`}> {/* ` バッククォートと ' シングルクォートの違いに注意*/}
+        <div className={`relative flex h-12 w-64 items-center rounded-full p-3 ${bgColor}`}> {/* ` バッククォートと ' シングルクォートの違いに注意*/}
             <div>
                 <Icon src={src} alt={alt} />
             </div>
-            <div className={`relative left-2 ${selected ? 'text-white' : 'text-default'}`}>
+            <div className={`relative left-2 ${textColor}`}>
                 {name}
             </div>
             <div className={`absolute left-52`}>
