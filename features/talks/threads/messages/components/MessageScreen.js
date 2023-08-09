81fff6a6949a582f087122b54158ba141b5e5943
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-10 00:01:51 +0900

    [ad]引数messagesに関する処理の実装

--- features/talks/threads/messages/components/MessageScreen.js
+++ features/talks/threads/messages/components/MessageScreen.js
@@ -4,20 +4,19 @@ import MessageTile from './MessageTile';
 
 /**
  * メッセージと返信欄を示すためのコンポーネント
- * @param
- * @param {string} src IconのURL
- * @param {string} value メッセージ内容
- * @param {string} createdAt メッセージ作成日時
- * @param {string} alt 画像が表示されなかった場合に表示される文字
  * @param {} onClickCreateReplyButton ボタンを押した時に動かす関数
  */
 
 export default function MessageScreen({messages,onClickCreateReplyButton}){
-
+    console.log(messages);
     return(
+
         <div>
-            {messages.map((massage)=> (
-                <MessageTile key={massage.id} value={massage.value} src={massage.src} alt={massage.alt} />
+            {messages.map((message)=> (
+                <div key={message.id} >
+                    <MessageTile createdAt={message.createdAt} value={message.value} src={message.src} alt={message.alt} />
+                    <div className='mb-3'/>
+                </div>
             ))}
         </div>
     );
