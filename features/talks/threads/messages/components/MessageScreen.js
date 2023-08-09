cc2484a1bdcbd8308f7240c17c2fb522150423c8
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-09 21:00:44 +0900

    [ad]MessageScreen.js枠組み作成

---
+++ features/talks/threads/messages/components/MessageScreen.js
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import MessageTile from './MessageTile';
+
+/**
+ * メッセージと返信欄を示すためのコンポーネント
+ * @param
+ */
+
+export default function MessageScreen({message,onClickCreateReplyButton}){
+    return(
+        <div>
+            aa
+        </div>
+    );
+}
