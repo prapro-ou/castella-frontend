e0e593d8981c49281e68f8b73238707cb7ff11d2
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-09 10:59:43 +0900

    [ad]Icon.jsにJSDocを追加

--- features/components/Icon.js
+++ features/components/Icon.js
@@ -1,4 +1,10 @@
 import Image from 'next/image';
+
+/**
+ * アイコンを表示するためのコンポーネント
+ * @param {} src 画像のURL
+ * @param {} slt 画像がないときに表示する文章
+ */
 export default function Icon({ src, alt }) {
   if (src === undefined)
   {
