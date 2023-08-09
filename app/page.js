9b1afca18b1e36f6bb161ef96de6659195f913b7
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-10 00:29:16 +0900

    [del]page.jsからテストコード削除

--- app/page.js
+++ app/page.js
@@ -1,15 +1,8 @@
 'use client';
-import { useState } from "react";
-import MessageScreen from "@/features/talks/threads/messages/components/MessageScreen";
 export default function Home() {
-  const messages =[
-    {id:1,value:"こんにちは，げんきかね",createdAt:"12:30"},
-    {id:2,value:"お疲れ様です。元気です",createdAt:"12:32"},
-  ];
 
   return (
     <main>
-      <MessageScreen messages={messages} />
       <p>
         LAページ
         <a href='/login' className='text-primary'>
