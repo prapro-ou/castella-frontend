22f8d4ed13223c3ccd2730aa9b1a899915c50788
Yuto-Kambara <135821880+Yuto-Kambara@users.noreply.github.com>
2023-08-10 04:35:10 +0900

    ページ部分の修正

--- app/page.js
+++ app/page.js
@@ -9,14 +9,6 @@ export default function Home() {
           Loginページ
         </a>
       </p>
-      <ThreadTile
-        subject={'明日の晩ご飯には煮物が出ます'}
-        body={'サンバのリズムを知ってるかい'}
-        messageCount={20}
-        isNew={null}
-        onClickMessageCount={20}
-        selected={true}
-      />
     </main>
   );
 }
