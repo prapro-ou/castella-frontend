e5a93db6b0eaa90429d6b89c08463d72a3e00e6a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-12 08:07:16 +0900

    [refactor] ThreadTileとWriteTextを統合

--- features/talks/threads/component/ThreadTile.js
+++ features/talks/threads/component/ThreadTile.js
@@ -8,15 +8,19 @@ export default function ThreadTile({
 }) {
   const bgColor = selected === true ? 'bg-primary' : 'bg-card';
   const numColor = selected === true ? 'text-white' : 'text-url';
+  const textColor = selected
+    ? 'text-white' : isNew
+      ? 'text-black' : 'text-thin';
+  const borderColor = selected ? 'border-white' : 'border-thin';
 
   return (
     <div className={`rounded-2xl w-72 h-28 ${bgColor}`}>
-      <WriteText
-        subject={subject}
-        body={body}
-        isNew={isNew}
-        selected={selected}
-      />
+      <div className={textColor}>
+        <p className={`text-xl pt-3 ml-3 pl-2 border-b w-64 + ${borderColor}`}>
+          {textOverflow(subject)}
+        </p>
+        <p className='text-base ml-5 mt-2'>{textOverflow(body)}</p>
+      </div>
       <div className='ml-auto mr-2 w-1/4 mt-2'>
         <button className='' onClick={onClickMessageCount}>
           <div className={`text-xs ${numColor}`}>{messageCount}件の返信</div>
@@ -26,22 +30,6 @@ export default function ThreadTile({
   );
 }
 
-function WriteText({ subject, body, isNew, selected }) {
-  const displaySubject =
-    subject.length > 9 ? subject.slice(0, 9) + '...' : subject;
-  const displayBody = body.length > 9 ? body.slice(0, 9) + '...' : body;
-  const textColor = selected
-    ? 'text-white'
-    : isNew
-    ? 'text-black'
-    : 'text-thin';
-  const borderColor = selected ? 'border-white' : 'border-thin';
-  return (
-    <div className={textColor}>
-      <p className={`text-xl pt-3 ml-3 pl-2 border-b w-64 + ${borderColor}`}>
-        {displaySubject}
-      </p>
-      <p className='text-base ml-5 mt-2'>{displayBody}</p>
-    </div>
-  );
+function textOverflow(value) {
+  return value.length > 20 ? value.slice(0, 20) + '...' : value;
 }
