bb3bdfb3c52085299a9d1b2ef11ba286e185bcca
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-14 08:56:09 +0900

    [fix] divの構造を変更

--- features/talks/threads/component/DestinationList.js
+++ features/talks/threads/component/DestinationList.js
@@ -5,12 +5,14 @@ export default function DestinationList(){
     const add_src='./add_black.svg';
 
     return(
-      <div className="flex w-64 items-center">
-        <div className="text-3xl">
-        DM
-        </div>
-        <div className="ml-auto">
-          <Icon src={add_src} alt="add icon" />
+      <div className="w-64">
+        <div className="flex items-center">
+          <div className="text-3xl">
+            DM
+          </div>
+          <div className="ml-auto">
+            <Icon src={add_src} alt="add icon" />
+          </div>
         </div>
       </div>
     );
