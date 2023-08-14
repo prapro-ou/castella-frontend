dd858a565f0efd66d336f2ef1d02d16697d147e4
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-14 18:55:04 +0900

    [format] prettierを実行

--- features/talks/threads/component/ThreadScreen.js
+++ features/talks/threads/component/ThreadScreen.js
@@ -1,22 +1,28 @@
-import FloatActionButton from "@/features/components/FloatActionButton"
-import ThreadTile from "./ThreadTile";
+import FloatActionButton from '@/features/components/FloatActionButton';
+import ThreadTile from './ThreadTile';
 
-export default function ThreadScreen ({threads, onClickCreateThreadBotton}){
-    const list = threads.map((threads,index) =>
-    <div key={index}><ThreadTile 
-    subject={threads.subject} 
-    body={threads.body} 
-    messageCount={threads.messageCount} 
-    isNew={threads.isNew} 
-    onClickMessageCount={threads.onClickMessageCount} 
-    selected={threads.selected}/>
+export default function ThreadScreen({ threads, onClickCreateThreadBotton }) {
+  const list = threads.map((threads, index) => (
+    <div key={index}>
+      <ThreadTile
+        subject={threads.subject}
+        body={threads.body}
+        messageCount={threads.messageCount}
+        isNew={threads.isNew}
+        onClickMessageCount={threads.onClickMessageCount}
+        selected={threads.selected}
+      />
     </div>
-    );
-    return(
-        <>
-            <div className=" grid-cols-1 space-y-10" >{list}</div>
-            <button className=" drop-shadow-xl fixed bottom-2 right-2 " onClick={onClickCreateThreadBotton}><FloatActionButton/></button>
-</>
-);
-
+  ));
+  return (
+    <>
+      <div className=' grid-cols-1 space-y-10'>{list}</div>
+      <button
+        className=' fixed bottom-2 right-2 drop-shadow-xl '
+        onClick={onClickCreateThreadBotton}
+      >
+        <FloatActionButton />
+      </button>
+    </>
+  );
 }
