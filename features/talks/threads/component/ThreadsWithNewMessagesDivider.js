d49a2fb66e191bb983223300586ce2081d7f3165
1daLearn <43876557+1daLearn@users.noreply.github.com>
2023-08-15 23:01:10 +0900

    removed mt-4,border-t etc

--- features/talks/threads/component/ThreadsWithNewMessagesDivider.js
+++ features/talks/threads/component/ThreadsWithNewMessagesDivider.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ThreadsWithNewMessagesDivider() {
   return (
-    <div className='flex items-center mt-4'>
-      <hr className='flex-grow border-t url' />
-      <span className='mx-4 url'>"新着メッセージ"</span>
+    <div className='flex items-center'>
+      <hr className='flex-grow url' />
+      <span className='mx-4 url'>新着メッセージ</span>
       <hr className='flex-grow border-t url' />
     </div>
   );
