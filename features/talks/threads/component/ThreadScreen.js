49a68acaa1cdefe639d18a4c7e00da162a6e8aca
Yuto-Kambara <135821880+Yuto-Kambara@users.noreply.github.com>
2023-08-15 20:26:45 +0900

    変数名の修正

--- features/talks/threads/component/ThreadScreen.js
+++ features/talks/threads/component/ThreadScreen.js
@@ -1,7 +1,7 @@
 import FloatActionButton from '@/features/components/FloatActionButton';
 import ThreadTile from './ThreadTile';
 
-export default function ThreadScreen({ threads, onClickCreateThreadBotton }) {
+export default function ThreadScreen({ threads, onClickCreateThreadButton }) {
   const list = threads.map((threads, index) => (
     <div key={index}>
       <ThreadTile
@@ -19,7 +19,7 @@ export default function ThreadScreen({ threads, onClickCreateThreadBotton }) {
       <div className='grid-cols-1 space-y-10'>{list}</div>
       <button
         className='fixed bottom-2 right-2 drop-shadow-xl '
-        onClick={onClickCreateThreadBotton}
+        onClick={onClickCreateThreadButton}
       >
         <FloatActionButton />
       </button>
