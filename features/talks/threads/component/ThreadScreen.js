cbd72385ebf991273024d3c3f36d457e66bc69b4
Yuto-Kambara <135821880+Yuto-Kambara@users.noreply.github.com>
2023-08-15 20:59:06 +0900

    ThreadTileの変数の書き方の変更

--- features/talks/threads/component/ThreadScreen.js
+++ features/talks/threads/component/ThreadScreen.js
@@ -4,14 +4,7 @@ import ThreadTile from './ThreadTile';
 export default function ThreadScreen({ threads, onClickCreateThreadButton }) {
   const list = threads.map((threads, index) => (
     <div key={index}>
-      <ThreadTile
-        subject={threads.subject}
-        body={threads.body}
-        messageCount={threads.messageCount}
-        isNew={threads.isNew}
-        onClickMessageCount={threads.onClickMessageCount}
-        selected={threads.selected}
-      />
+        {ThreadTile (threads)}
     </div>
   ));
   return (
@@ -19,7 +12,6 @@ export default function ThreadScreen({ threads, onClickCreateThreadButton }) {
       <div className='grid-cols-1 space-y-10'>{list}</div>
       <button
         className='fixed bottom-2 right-2 drop-shadow-xl '
-        onClick={onClickCreateThreadButton}
       >
         <FloatActionButton />
       </button>
