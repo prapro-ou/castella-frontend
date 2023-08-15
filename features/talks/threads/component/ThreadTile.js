e458566c337a42fb721163eeae2a51255cc41c5e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-16 08:51:58 +0900

    [delete] 余計な文を削除

--- features/talks/threads/component/ThreadTile.js
+++ features/talks/threads/component/ThreadTile.js
@@ -36,37 +36,6 @@ export default function ThreadTile({
   );
 }
 
-<<<<<<<<< Temporary merge branch 1
 function textOverflow(value) {
   return value.length > 20 ? value.slice(0, 20) + '...' : value;
-=========
-function WriteText({ subject, body, isNew, selected }) {
-  const displaySubject =
-    subject.length > 9 ? subject.slice(0, 9) + '...' : subject;
-  const displayBody = body.length > 9 ? body.slice(0, 9) + '...' : body;
-  const textColor = selected
-    ? 'text-white'
-    : isNew
-    ? 'text-black'
-    : 'text-thin';
-  const borderColor = selected ? 'border-white' : 'border-thin';
-  return (
-    <div className={textColor}>
-      <p className={`text-xl pt-3 ml-3 pl-2 border-b w-64 + ${borderColor}`}>
-        {displaySubject}
-      </p>
-      <p className='text-base ml-5 mt-2'>{displayBody}</p>
-    </div>
-  );
-}
-
-function NumberOfReplies({ messageCount, onClickMessageCount, selected }) {
-  const numColor = selected == true ? 'text-white' : 'text-url';
-
-  return (
-    <button className='' onclick={onClickMessageCount}>
-      <div className={`text-xs ${numColor}`}>{messageCount}件の返信</div>
-    </button>
-  );
->>>>>>>>> Temporary merge branch 2
 }
