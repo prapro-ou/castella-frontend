ddf16321000b783af74f2380cf256b370f8cf6f2
1daLearn <43876557+1daLearn@users.noreply.github.com>
2023-08-15 23:03:20 +0900

    change color url->border-gray-500

--- features/talks/threads/component/ThreadsWithNewMessagesDivider.js
+++ features/talks/threads/component/ThreadsWithNewMessagesDivider.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function ThreadsWithNewMessagesDivider() {
   return (
     <div className='flex items-center'>
-      <hr className='flex-grow url' />
-      <span className='mx-4 url'>新着メッセージ</span>
-      <hr className='flex-grow border-t url' />
+      <hr className='flex-grow border-gray-500' />
+      <span className='mx-4 border-gray-500'>新着メッセージ</span>
+      <hr className='flex-grow border-t border-gray-500' />
     </div>
   );
 }
