cfd94ed9720fb3203af893c5d2a6c5fb2a44c836
1daLearn <43876557+1daLearn@users.noreply.github.com>
2023-08-16 12:45:35 +0900

    color changed blue_500->url

--- features/talks/threads/component/ThreadsWithNewMessagesDivider.js
+++ features/talks/threads/component/ThreadsWithNewMessagesDivider.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function ThreadsWithNewMessagesDivider() {
   return (
     <div className='flex items-center'>
-      <hr className='flex-grow border-blue-500' />
-      <span className='mx-4 border-blue-500'>新着メッセージ</span>
-      <hr className='flex-grow border-t border-blue-500' />
+      <hr className='flex-grow border-url' />
+      <span className='mx-4 border-url'>新着メッセージ</span>
+      <hr className='flex-grow border-t border-url' />
     </div>
   );
 }
