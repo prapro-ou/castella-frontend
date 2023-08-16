48b0036fe212c5f1785771882ceef77e2abcc43a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-17 02:18:13 +0900

    [format] prettierを実行

--- features/talks/threads/component/ThreadsWithNewMessagesDivider.js
+++ features/talks/threads/component/ThreadsWithNewMessagesDivider.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function ThreadsWithNewMessagesDivider() {
   return (
     <div className='flex items-center'>
-      <hr className='flex-grow border-url' />
+      <hr className='grow border-url' />
       <span className='mx-4 text-url'>新着メッセージ</span>
-      <hr className='flex-grow border-t border-url' />
+      <hr className='grow border-t border-url' />
     </div>
   );
 }
