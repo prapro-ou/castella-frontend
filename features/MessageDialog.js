4b9fa6d8390e1b2182907ba691c069165c2e0043
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-18 17:32:13 +0900

    [add+del] Dialogのテンプレートを呼び出す

--- features/MessageDialog.js
+++ features/MessageDialog.js
@@ -1,8 +1,48 @@
-import React from "react";
-import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { Button,TextField,Dialog,DialogActions,DialogContent,DialogContentText,DialogTitle } from "@mui/material";
+import styled from "@emotion/styled";
 
 export default function MessageDialog(){
-    return(
-        <Button variant="contained">contained</Button>
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    let [email, setEmail] = useState("");
+  
+    return (
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Open form dialog
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>New reply</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please write a new reply here!
+            </DialogContentText>
+            <TextField
+              autoFocus={true}
+              margin="dense"
+              id="message"
+              label="body"
+              type="email"
+              fullWidth
+              value={email}
+              onChange={(value)=>{setEmail(value);}}
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Send</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     );
 }
