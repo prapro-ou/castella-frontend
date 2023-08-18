c0dd8c6bea63676dda53bc25073c3f0447c84362
Yuto-Kambara <135821880+Yuto-Kambara@users.noreply.github.com>
2023-08-18 17:11:20 +0900

    [add] loginページのHTMLを追加

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,23 @@
 export default function Login() {
   return (
     <main>
-      <h1>Loginページ</h1>
+      <p>
+        Loginページ
+        <a href='/talks' className='text-primary'>
+          Login
+        </a>
+      </p>
+      <form method="POST" action=""> 
+        <div>
+          <label for="email">メールアドレス</label>
+          <input className="ring-2" type="mail" id="emial" name="email" />
+        </div>
+        <div>
+          <label for="password">パスワード</label>
+          <input className="ring-2" type="password" id="password" name="password"/>
+        </div>
+        <button className = "bg-thin">送信</button>
+      </form>
     </main>
   );
 }
