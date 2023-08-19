a40330a9c90d5d72a7e153a9ec641ec450a57615
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-19 18:37:35 +0900

    [add] 確認用のコードを追加

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
 'use client';
+
+import MessageDialog from "@/features/MessageDialog";
+import FloatActionButton from "@/features/components/FloatActionButton";
+import addBlack from '@/public/addBlack.svg';
+import { useState } from "react";
+
 export default function Home() {
+  const[open,setOpen]=useState(false);
+  function onClickSend(text){
+    console.log(text);
+  }
+
   return (
     <main>
       <p>
-        LAページ
-        <a href='/login' className='text-primary'>
+        <a>
           Loginページ
         </a>
       </p>
+      <FloatActionButton src={addBlack} alt="add" onClickButton={()=>setOpen(true)} />
+      <MessageDialog open={open} setOpen={setOpen} onClickSend={onClickSend} />
     </main>
   );
 }
