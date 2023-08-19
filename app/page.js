8ef9d4b469dcc0e2e96d43fa694e2e03ce1120d7
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-19 19:14:01 +0900

    [fux] 確認コードを変更

--- app/page.js
+++ app/page.js
@@ -7,15 +7,17 @@ import { useState } from "react";
 
 export default function Home() {
   
-  const [openDialog,setOpenDialog]=useState(false);
+  const [openDialog,setOpenDialog]=useState(true);
 
   function onClickAdd(){
+    console.log("Add Clicked");
     setOpenDialog(true);
   }
   function onClickCancel(){
-    console.log("Canceled");
+    console.log("Cancel Clicked");
   }
   function onClickSend(text){
+    console.log("Send Clicked");
     console.log(text);
   }
 
