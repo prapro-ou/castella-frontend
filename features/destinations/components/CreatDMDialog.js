0004d4a2356780bdd64e66c40b26bacb942775ef
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-20 12:42:04 +0900

    [add + fix] ThreadDialogを複製して関数名を変更

---
+++ features/destinations/components/CreatDMDialog.js
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from "react";
+import { Button,TextField,Dialog,DialogActions,DialogContent,DialogTitle } from "@mui/material";
+
+export default function CreatDMDialog({isOpened,onClickCancel,onClickSend}){
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+ 
+    return (
+      <div>
+        <Dialog open={isOpened}>
+          <DialogTitle>新規スレッド</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus={true}
+              margin="dense"
+              id="subject"
+              label="件名"
+              type="text"
+              fullWidth
+              value={subject}//前回の入力を記憶
+              onChange={(event)=> setSubject(event.target.value)}
+              variant="standard"
+            />
+            <TextField
+              margin="dense"
+              id="message"
+              label="本文"
+              type="text"
+              fullWidth
+              value={message}//前回の入力を記憶
+              onChange={(event)=> setMessage(event.target.value)}
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={()=>{onClickCancel();}}>閉じる</Button>
+            <Button onClick={()=>{onClickSend(subject,message);}}>送信</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+}
