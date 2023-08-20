cc78d4be7e8db8b8d5ec97af0200689a84363ad9
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-20 10:52:21 +0900

    [del] LAページの差分

--- app/page.js
+++ app/page.js
@@ -1,28 +1,5 @@
 'use client';
-
-import MessageDialog from "@/features/MessageDialog";
-import FloatActionButton from "@/features/components/FloatActionButton";
-import addBlack from '@/public/addBlack.svg';
-import { useState } from "react";
-
 export default function Home() {
-  
-  const [openDialog,setOpenDialog]=useState(false);
-
-  function onClickAdd(){
-    console.log("Add Clicked");
-    console.log(openDialog);
-    setOpenDialog(true);
-    console.log(openDialog);
-  }
-  function onClickCancel(){
-    console.log("Cancel Clicked");
-  }
-  function onClickSend(text){
-    console.log("Send Clicked");
-    console.log(text);
-  }
-
   return (
     <main>
       <p>
@@ -31,8 +8,6 @@ export default function Home() {
           Loginページ
         </a>
       </p>
-      <FloatActionButton src={addBlack} alt="add" onClickButton={onClickAdd} />
-      <MessageDialog isOpened={openDialog} onClickCancel={onClickCancel} onClickSend={onClickSend} />
     </main>
   );
 }
