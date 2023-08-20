856b9263ffc5bdf8d48bc0cb50153530f8500c8b
Yuto-Kambara <135821880+Yuto-Kambara@users.noreply.github.com>
2023-08-20 12:04:20 +0900

    環境変数とcookieにtokenを保存する機構の追加

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { cookies } from "next/dist/client/components/headers";
 import { useState } from "react";
 import 'whatwg-fetch';
 
@@ -17,29 +18,44 @@ export default function Login() {
           <label>パスワード</label>
           <input className="ring-2" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
         </div>
-        <button onClick={()=>{
-          fetch('https://3385-113-197-158-64.ngrok-free.app/login', {
-            method: 'POST',
-            mode:"cors",
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              email: email,
-              password: password,
-            })
-          }).then(function(response) {
-            console.log(response);
-            return response.json();
-          })
-          .then(function(data){
-            console.log(data);
-          })
-         .catch(function(error) {
-            console.error(error);
-          });
-        }} className = "bg-thin">送信</button>
+        <button onClick={()=>onClickLoginPageSendButton({email,password})} className = "bg-thin">送信</button>
       </div>
     </main>
   );
+}
+
+function onClickLoginPageSendButton({email,password}){
+  fetch(process.env.NEXT_PUBLIC_URL+'/login', {
+    method: 'POST',
+    mode:"cors",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    })
+  }).then(function(response) {
+    return response.json();
+  })
+  .then(function(data){
+    if (!navigator.cookieEnabled) {
+      alert('cookieを有効にしてください');
+      return;
+    };
+
+    // cookieに情報がない場合
+    if(!document.cookie) {
+      document.cookie = JSON.stringify({'token': data.token});
+    }
+    // cookieに情報がある場合
+    else {
+      const cookieAsJson = JSON.parse(document.cookie);
+      cookieAsJson.token = data.token;
+      document.cookie = JSON.stringify(cookieAsJson);
+    }
+  })
+ .catch(function(error) {
+    console.error(error);
+ });
 }
