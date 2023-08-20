52aaf8bbf4d7f4d0b7bcf7e3492d2a9ac1cbddfe
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-20 17:42:35 +0900

    [format] prettierを実行

--- app/login/page.js
+++ app/login/page.js
@@ -1,33 +1,47 @@
-"use client";
+'use client';
 
-import postLoginRequest from "@/features/auth/data/PostLoginRequest";
-import { useState } from "react";
+import postLoginRequest from '@/features/auth/data/PostLoginRequest';
+import { useState } from 'react';
 import 'whatwg-fetch';
 import { useRouter } from 'next/navigation';
 
 export default function Login() {
   const router = useRouter();
-  let [email, setEmail] = useState("");
-  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
   return (
     <main>
-      <div> 
+      <div>
         <div>
           <label>メールアドレス</label>
-          <input className="ring-2" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+          <input
+            className='ring-2'
+            type='email'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
         <div>
           <label>パスワード</label>
-          <input className="ring-2" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+          <input
+            className='ring-2'
+            type='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
-        <button onClick={async ()=> {
-          const isSuccess = await postLoginRequest(email, password);
-          if (isSuccess) {
-            router.push(`/destinations`);
-          }
-          }} className = "bg-thin">送信</button>
+        <button
+          className='bg-thin'
+          onClick={async () => {
+            const isSuccess = await postLoginRequest(email, password);
+            if (isSuccess) {
+              router.push(`/destinations`);
+            }
+          }}
+        >
+          送信
+        </button>
       </div>
     </main>
   );
 }
-
--- app/register/page.js
+++ app/register/page.js
@@ -1,39 +1,61 @@
-"use client";
+'use client';
 
-import postLoginRequest from "@/features/auth/data/PostLoginRequest";
-import PostRegisterRequest from "@/features/auth/data/PostRegisterRequest";
-import { useState } from "react";
+import postLoginRequest from '@/features/auth/data/PostLoginRequest';
+import PostRegisterRequest from '@/features/auth/data/PostRegisterRequest';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Register() {
   const router = useRouter();
-  let [email, setEmail] = useState("");
-  let [loginPassword, setLoginPassword] = useState("");
-  let [emailPassword, setEmailPassword] = useState("");
+  let [email, setEmail] = useState('');
+  let [loginPassword, setLoginPassword] = useState('');
+  let [emailPassword, setEmailPassword] = useState('');
   return (
     <main>
-      <div> 
+      <div>
         <div>
           <label>メールアドレス</label>
-          <input className="ring-2" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+          <input
+            className='ring-2'
+            type='email'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
         <div>
           <label>ログインパスワード</label>
-          <input className="ring-2" type="password" value={loginPassword} onChange={(event) => setLoginPassword(event.target.value)}/>
+          <input
+            className='ring-2'
+            type='password'
+            value={loginPassword}
+            onChange={(event) => setLoginPassword(event.target.value)}
+          />
         </div>
         <div>
           <label>メールパスワード</label>
-          <input className="ring-2" type="password" value={emailPassword} onChange={(event) => setEmailPassword(event.target.value)}/>
+          <input
+            className='ring-2'
+            type='password'
+            value={emailPassword}
+            onChange={(event) => setEmailPassword(event.target.value)}
+          />
         </div>
-        <button onClick={async ()=> {
-             const isRegisterSuccess = await PostRegisterRequest(email,loginPassword,emailPassword);
-              if(!isRegisterSuccess)
-              return; 
-              const isLoginSuccess = postLoginRequest(email, loginPassword);
-              if (!isLoginSuccess)
-              return;
-              router.push(`/destinations`);
-          }} className = "bg-thin">送信</button>
+        <button
+          onClick={async () => {
+            const isRegisterSuccess = await PostRegisterRequest(
+              email,
+              loginPassword,
+              emailPassword,
+            );
+            if (!isRegisterSuccess) return;
+            const isLoginSuccess = postLoginRequest(email, loginPassword);
+            if (!isLoginSuccess) return;
+            router.push(`/destinations`);
+          }}
+          className='bg-thin'
+        >
+          送信
+        </button>
       </div>
     </main>
   );
