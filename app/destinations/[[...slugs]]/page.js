fdc32dea02bb03b5d0ca21112965c6bea2bd2188
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-20 19:17:38 +0900

    [?] 先輩によって完成

--- app/destinations/[[...slugs]]/page.js
+++ app/destinations/[[...slugs]]/page.js
@@ -1,38 +1,36 @@
+'use client';
+
 import Tips from "@/features/components/Tips";
+import CreatDMDialog from "@/features/destinations/components/CreatDMDialog";
 import DestinationList from "@/features/destinations/components/DestinationList";
+import getDestinationsRequest from "@/features/destinations/data/GetDestinationsRequest";
+import postDestinationsRequest from "@/features/destinations/data/PostDestinationsRequest";
 import { useEffect, useState } from "react";
 
 
 export default function Destinations() {
-  const [dests,setDests]=useState([]);
+  const [dms, setDms]=useState([]);
+  const [openCreatDMDialog,setOpenCreatDMDialog]=useState(false);
+
   useEffect(()=>{
-    const cookieAsJson = JSON.parse(document.cookie);
-    fetch(`${process.env.NEXT_PUBLIC_URL}/destinations`,{
-      method:'GET',
-      mode:'cors',
-      headers:{
-        'Authorization': `Bearer ${cookieAsJson.token}`,
-      },})
-      .then(function (response){
-        console.log(response);
-        return response.json();
-      })
-      .then( function (data){
-        console.log(data.dms);
-        setDests(data.dms);
-      })
-      .catch(function (error){
-        console.log(error);
-      });
+    setInterval(async () => {
+      const json = await getDestinationsRequest();
+      setDms(json.dms);
+    }, 5000);
   },[]);
-  function clickTest(){
-    console.log("Add Clicked");
-  }
+
 
   return (
     <div className="flex h-screen bg-gray">
       <div className="w-72 bg-white">
-        <DestinationList destinations={dests} onClickAddButton={clickTest} />
+        <DestinationList destinations={dms} onClickAddButton={()=>{setOpenCreatDMDialog(true);}} />
+        <CreatDMDialog
+         isOpened={openCreatDMDialog}
+          onClickCancel={()=>{setOpenCreatDMDialog(false);}}
+          onClickSend={(name, to) => {
+            postDestinationsRequest(name, to);
+            setOpenCreatDMDialog(false);
+          }} />
       </div>
       <div className="m-auto">
         <Tips />
