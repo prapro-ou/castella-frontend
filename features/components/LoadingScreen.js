93dbf7e188a2055f269737c4e1f96d23bd16f00a
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-22 23:34:34 +0900

    [ad]ライブラリを用いてローディングを表示

--- features/components/LoadingScreen.js
+++ features/components/LoadingScreen.js
@@ -1,8 +1,27 @@
 'use client';
-import { Audio } from  'react-loader-spinner';
+import { Oval } from  'react-loader-spinner';
 
+/**
+ * ローディング画面表示するコンポーネント
+ * @param {boolean} isLoading ローディング中かどうか
+ * @author Jaja-UMA
+ */
 export default function LoadingScreen({isLoading}){
+    
     return(
-        <div></div>
+        <div>
+        <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel='oval-loading'
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+        />
+        </div>
     );
 }
