d41ac1128fb4fa9a6214ff197761178137870f44
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-23 04:32:03 +0900

    [add] labelとinputの関連付け

--- app/register/page.js
+++ app/register/page.js
@@ -14,28 +14,31 @@ export default function Register() {
     <main>
       <div>
         <div>
-          <label>メールアドレス</label>
+          <label htmlFor='address'>メールアドレス</label>
           <input
             className='ring-2'
             type='email'
+            id='address'
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
         </div>
         <div>
-          <label>ログインパスワード</label>
+          <label htmlFor='loginPassword'>ログインパスワード</label>
           <input
             className='ring-2'
             type='password'
+            id='loginPassword'
             value={loginPassword}
             onChange={(event) => setLoginPassword(event.target.value)}
           />
         </div>
         <div>
-          <label>メールパスワード</label>
+          <label htmlFor='mailPassword'>メールパスワード</label>
           <input
             className='ring-2'
             type='password'
+            id='mailPassword'
             value={emailPassword}
             onChange={(event) => setEmailPassword(event.target.value)}
           />
