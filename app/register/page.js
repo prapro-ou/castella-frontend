310eb4f80c5c806f962f68e0929e898885192cdd
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-23 07:15:30 +0900

    [format] prettier

--- app/register/page.js
+++ app/register/page.js
@@ -1,65 +1,65 @@
-'use client';
+"use client";
 
-import React from 'react';
-import postLoginRequest from '@/features/auth/data/PostLoginRequest';
-import PostRegisterRequest from '@/features/auth/data/PostRegisterRequest';
-import { Button , TextField } from '@mui/material';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import postLoginRequest from "@/features/auth/data/PostLoginRequest";
+import PostRegisterRequest from "@/features/auth/data/PostRegisterRequest";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
   const router = useRouter();
-  let [email, setEmail] = useState('');
-  let [loginPassword, setLoginPassword] = useState('');
-  let [emailPassword, setEmailPassword] = useState('');
+  let [email, setEmail] = useState("");
+  let [loginPassword, setLoginPassword] = useState("");
+  let [emailPassword, setEmailPassword] = useState("");
   return (
     <main>
-      <div className='grid h-screen w-screen items-center justify-center'>
-      <div className='grid h-fit w-fit space-y-3'>
-        <TextField
-          label="メールアドレス"
-          id='address'
-          type='email'
-          variant="outlined"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <TextField
-          label="ログインパスワード"
-          id='loginPassword'
-          variant="outlined" 
-          type='password'
-          value={loginPassword}
-          onChange={(event) => setLoginPassword(event.target.value)}
-        />
-        <TextField
-          label="メールパスワード"
-          id='mailPassword'
-          variant="outlined"
-          type='password'
-          value={emailPassword}
-          onChange={(event) => setEmailPassword(event.target.value)}          
-        />
-        <Button
-          variant='contained'
-          className='bg-primary'
-          color='success'
-          onClick={async () => {
-            const isSuccessToRegister = await PostRegisterRequest(
-              email,
-              loginPassword,
-              emailPassword,
-            );
-            if (!isSuccessToRegister) return;
-            const isSuccessToLogin = postLoginRequest(email, loginPassword);
-            if (!isSuccessToLogin) return;
-            router.push(`/destinations`);
-          }}
-        >
-          送信
-        </Button>
-      </div>
-      <div className='h-1/4'>{/* 上の要素の位置調整のための余白 */}</div>
+      <div className="grid h-screen w-screen items-center justify-center">
+        <div className="grid h-fit w-fit space-y-3">
+          <TextField
+            label="メールアドレス"
+            id="address"
+            type="email"
+            variant="outlined"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <TextField
+            label="ログインパスワード"
+            id="loginPassword"
+            variant="outlined"
+            type="password"
+            value={loginPassword}
+            onChange={(event) => setLoginPassword(event.target.value)}
+          />
+          <TextField
+            label="メールパスワード"
+            id="mailPassword"
+            variant="outlined"
+            type="password"
+            value={emailPassword}
+            onChange={(event) => setEmailPassword(event.target.value)}
+          />
+          <Button
+            variant="contained"
+            className="bg-primary"
+            color="success"
+            onClick={async () => {
+              const isSuccessToRegister = await PostRegisterRequest(
+                email,
+                loginPassword,
+                emailPassword
+              );
+              if (!isSuccessToRegister) return;
+              const isSuccessToLogin = postLoginRequest(email, loginPassword);
+              if (!isSuccessToLogin) return;
+              router.push(`/destinations`);
+            }}
+          >
+            送信
+          </Button>
+        </div>
+        <div className="h-1/4">{/* 上の要素の位置調整のための余白 */}</div>
       </div>
     </main>
   );
