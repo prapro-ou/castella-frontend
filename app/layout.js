5f1e289bb4996a1266da67c8c91f1e019a4ecc78
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-23 11:41:02 +0900

    [fix] 差分を削除

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className + " overflow-hidden"}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
