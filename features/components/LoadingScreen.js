0fec5a4b1ace4b4394e3b40c638c412a904c0ef8
Jaja-UMA <73741714+Jaja-UMA@users.noreply.github.com>
2023-08-23 11:09:43 +0900

    [fix]コード整形

--- features/components/LoadingScreen.js
+++ features/components/LoadingScreen.js
@@ -13,20 +13,21 @@ export default function LoadingScreen({isLoading}){
     useEffect(() =>{
         setLoading(isLoading);
     },[isLoading]);
+
     return(
         <div>
-        { loading && <Oval
-        height={80}
-        width={80}
-        color="#4fa94d"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel='oval-loading'
-        secondaryColor="#4fa94d"
-        strokeWidth={2}
-        strokeWidthSecondary={2}
-        />
+        {loading && <Oval
+            height={80}
+            width={80}
+            color="#4fa94d"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel='oval-loading'
+            secondaryColor="#4fa94d"
+            strokeWidth={2}
+            strokeWidthSecondary={2}
+                    />
         }
         </div>
     );
