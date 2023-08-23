03252e771ccdaa88ca26d23abefbba1c0b515816
KitauchiRyota <136436225+KitauchiRyota@users.noreply.github.com>
2023-08-23 09:25:50 +0900

    [add + fix] 左部分 位置調整

--- app/register/page.js
+++ app/register/page.js
@@ -25,55 +25,61 @@ export default function Register() {
   return (
     <main>
       <ThemeProvider theme={theme}>
-        <div className="grid h-screen w-screen items-center justify-center">
-          <div className="grid h-fit w-fit space-y-3">
-            <TextField
-              label="メールアドレス"
-              id="address"
-              type="email"
-              color="castella"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              variant="outlined"
-            />
-            <TextField
-              label="ログインパスワード"
-              id="loginPassword"
-              type="password"
-              color="castella"
-              value={loginPassword}
-              onChange={(event) => setLoginPassword(event.target.value)}
-              variant="outlined"
-            />
-            <TextField
-              label="メールパスワード"
-              id="mailPassword"
-              type="password"
-              color="castella"
-              value={emailPassword}
-              onChange={(event) => setEmailPassword(event.target.value)}
-              variant="outlined"
-            />
-            <Button
-              variant="contained"
-              className="bg-primary"
-              color="castella"
-              onClick={async () => {
-                const isSuccessToRegister = await PostRegisterRequest(
-                  email,
-                  loginPassword,
-                  emailPassword
-                );
-                if (!isSuccessToRegister) return;
-                const isSuccessToLogin = postLoginRequest(email, loginPassword);
-                if (!isSuccessToLogin) return;
-                router.push(`/destinations`);
-              }}
-            >
-              送信
-            </Button>
+        <div className="flex">
+          <div className="h-screen w-2/3 items-center rounded-r-3xl bg-primary"></div>
+          <div className="grid h-screen w-full items-center justify-center">
+            <div className="grid h-fit w-fit space-y-3">
+              <TextField
+                label="メールアドレス"
+                id="address"
+                type="email"
+                color="castella"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                variant="outlined"
+              />
+              <TextField
+                label="ログインパスワード"
+                id="loginPassword"
+                type="password"
+                color="castella"
+                value={loginPassword}
+                onChange={(event) => setLoginPassword(event.target.value)}
+                variant="outlined"
+              />
+              <TextField
+                label="メールパスワード"
+                id="mailPassword"
+                type="password"
+                color="castella"
+                value={emailPassword}
+                onChange={(event) => setEmailPassword(event.target.value)}
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                className="bg-primary"
+                color="castella"
+                onClick={async () => {
+                  const isSuccessToRegister = await PostRegisterRequest(
+                    email,
+                    loginPassword,
+                    emailPassword
+                  );
+                  if (!isSuccessToRegister) return;
+                  const isSuccessToLogin = postLoginRequest(
+                    email,
+                    loginPassword
+                  );
+                  if (!isSuccessToLogin) return;
+                  router.push(`/destinations`);
+                }}
+              >
+                送信
+              </Button>
+            </div>
+            {/* <div></div> */}
           </div>
-          <div>{/* 上の要素の位置調整のための余白 */}</div>
         </div>
       </ThemeProvider>
     </main>
