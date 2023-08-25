8bba88b5a737a608d6a7c3fff60ee6923a3c8f1e
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-25 21:50:41 +0900

    [add] fakeDMThreadを追加

--- features/threads/data/GetDMThreadsRequest.js
+++ features/threads/data/GetDMThreadsRequest.js
@@ -1,4 +1,4 @@
-export default async function getDMThreadsRequest(dmId) {
+async function getDMThreadsRequest(dmId) {
   const cookieAsJson = JSON.parse(document.cookie);
   return fetch(`${process.env.NEXT_PUBLIC_URL}/destinations/dms/${dmId}`, {
     method: 'GET',
@@ -16,4 +16,37 @@ export default async function getDMThreadsRequest(dmId) {
     .catch(function (error) {
       console.log(error);
     });
+}
+
+// eslint-disable-next-line no-undef
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function fakeGetDMThreadsRequest() {
+  await sleep(2000);
+  return [
+    {
+      "id": "message_id1",
+      "subject": "件名1",
+      "body": "本文1",
+      "created_at": "2010-06-01T22:19:44.475Z",
+      "new_message_count": "0",
+      "reply_count": 0
+    },
+    {
+      "id": "message_id2",
+      "subject": "件名2",
+      "body": "本文2",
+      "created_at": "2010-06-01T22:19:44.475Z",
+      "new_message_count": "1",
+      "reply_count": 1
+    },
+    {
+      "id": "message_id3",
+      "subject": "件名3",
+      "body": "本文3",
+      "created_at": "2010-06-01T22:19:44.475Z",
+      "new_message_count": "2",
+      "reply_count": 2
+    }
+  ];
 }
