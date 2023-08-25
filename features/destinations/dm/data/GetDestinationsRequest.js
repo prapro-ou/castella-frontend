9f22ea65486355b1338211cc7097be1cd2c93606
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-25 20:51:15 +0900

    [add] destination向けのfakeを追加

--- features/destinations/dm/data/GetDestinationsRequest.js
+++ features/destinations/dm/data/GetDestinationsRequest.js
@@ -1,4 +1,4 @@
-export default async function getDestinationsRequest() {
+async function getDestinationsRequest() {
   const cookieAsJson = JSON.parse(document.cookie);
   return fetch(`${process.env.NEXT_PUBLIC_URL}/destinations`, {
     method: 'GET',
@@ -13,4 +13,47 @@ export default async function getDestinationsRequest() {
     .catch(function (error) {
       console.log(error);
     });
+}
+
+// eslint-disable-next-line no-undef
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function fakeGetDestinationsRequest() {
+  await sleep(2000);
+  return {
+    dms: [
+      {
+        id: "dmId1",
+        name: "name1",
+        new_message_count: 0
+      },
+      {
+        id: "dmId2",
+        name: "name2",
+        new_message_count: 1
+      },
+      {
+        id: "dmId3",
+        name: "name3",
+        new_message_count: 2
+      }
+    ],
+    "groups": [
+      {
+        "id": "groupId1",
+        "name": "name1",
+        "new_message_count": 0
+      },
+      {
+        "id": "groupId2",
+        "name": "name2",
+        "new_message_count": 1
+      },
+      {
+        "id": "groupId3",
+        "name": "name3",
+        "new_message_count": 2
+      }
+    ]
+  };
 }
